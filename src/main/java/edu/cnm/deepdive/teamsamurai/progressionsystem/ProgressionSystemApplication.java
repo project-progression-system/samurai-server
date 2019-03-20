0cfcf8ae295107322b37e942c8e5f48147514a9a
Alex Rauenzahn <xandermv@yahoo.com>
2019-03-20 17:01:11 -0600

    Initial commit.

---
+++ src/main/java/edu/cnm/deepdive/teamsamurai/progressionsystem/ProgressionSystemApplication.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.teamsamurai.progressionsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgressionSystemApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProgressionSystemApplication.class, args);
+  }
+
+}
